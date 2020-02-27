27b8295c0e1375d6656f32d28b8b584f06439693
orcuntuna <turboindirblog@gmail.com>
2020-02-27 16:36:54 +0300

    öğrenci profili sekmeler yapıldı, içerikleri doldurulacak

--- src/components/profile/StudentProfileTabs.js
+++ src/components/profile/StudentProfileTabs.js
@@ -1,11 +1,34 @@
 import React from "react";
-import {Icon, Button} from "antd";
+import {Icon, Tabs} from "antd";
+
+const { TabPane } = Tabs;
 
 export default class StudentProfileTabs extends React.Component {
     render() {
         return (
             <div className="col-md-9 col-xl-10 profile-right">
-                student info tabs
+                <div className="tabs">
+                    <Tabs defaultActiveKey="1" onChange={(key) => console.log(key)}>
+                        <TabPane tab="Kişisel Bilgiler" key="1">
+                            kişisel bilgilerin içi
+                        </TabPane>
+                        <TabPane tab="Eğitimler" key="2">
+                            Content of Tab Pane 2
+                        </TabPane>
+                        <TabPane tab="Deneyimler" key="3">
+                            Content of Tab Pane 3
+                        </TabPane>
+                        <TabPane tab="Sertifikalar" key="4">
+                            Content of Tab Pane 3
+                        </TabPane>
+                        <TabPane tab="Sertifikalar" key="5">
+                            Yetenekler
+                        </TabPane>
+                        <TabPane tab="Projeler" key="6">
+                            Content of Tab Pane 3
+                        </TabPane>
+                    </Tabs>
+                </div>
             </div>
         );
     }
